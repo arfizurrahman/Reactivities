23a7f5702bf190f6fc3ff019fc460092cab23c87
Arfiz <arfizur.rahman@vivacomsolutions.com>
2020-12-10 21:13:34 +0600

    Integrated client app with backend

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import { Header, List } from 'semantic-ui-react'
+interface Value {
+  id: number;
+  name: string;
+}
 
 function App() {
+
+  const data: Value[] = [];
+  const [values, setValues] = useState(data);
+
+  useEffect(()=> {
+    axios.get('http://localhost:5000/api/values').then((response) => {
+      setValues(response.data)
+    });
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+         {values.map((value: Value) => (
+           <List.Item key={value.id}>{value.name}</List.Item>
+         ))}
+        </List>
     </div>
   );
 }
