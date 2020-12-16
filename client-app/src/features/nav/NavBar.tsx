0face78f99a5e0b5fb17b6024fbb46a543a46e47
Arfiz <arfizur.rahman@vivacomsolutions.com>
2020-12-16 20:09:21 +0600

    Integrated mobx in client app

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
+import { observer } from 'mobx-react-lite';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import { useStore } from '../../app/stores/store';
 
-interface IProps {
-    openCreateForm: () => void;
-}
-const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+const NavBar: React.FC = () => {
+    const { activityStore } = useStore();
     return (
         <Menu fixed='top' inverted>
             <Container>
-            <Menu.Item header>
-                <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
                 Reactivities
             </Menu.Item>
-            
-            <Menu.Item
-            name='activities'
-            />
-            <Menu.Item>
-                <Button onClick={openCreateForm} positive content="Create Activity"/>
-            </Menu.Item>
+
+                <Menu.Item
+                    name='activities'
+                />
+                <Menu.Item>
+                    <Button onClick={activityStore.openCreateForm} type='button' positive content="Create Activity" />
+                </Menu.Item>
             </Container>
-      </Menu>
+        </Menu>
     )
 }
 
-export default NavBar;
+export default observer(NavBar);
