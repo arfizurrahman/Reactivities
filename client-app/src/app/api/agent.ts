590ebd22dd8f807df23f34d86eb738eee1a762a1
Arfiz <arfizrahman0@gmail.com>
2021-01-01 07:24:26 +0600

    Implemented edit profile

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -81,6 +81,7 @@ const Profiles = {
     uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => requests.delete(`/photos/${id}`),
+    updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`, profile)
 }
 
 const agent = {
